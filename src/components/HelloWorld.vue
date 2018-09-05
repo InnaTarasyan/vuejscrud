421d88f3fc8c4e616aa7920565e24f743dd59dc7
Inna Tarasyan <innatarasyanmail@gmail.com>
2018-09-06 01:37:34 +0500

    code enhancement

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,14 @@
 
 <template>
-  <div>
-    <b-table striped hover :items="items" :fields="fields"  :current-page="currentPage" :per-page="perPage"></b-table>
-    <b-pagination align="center" size="md" :total-rows="items.length" :per-page="perPage" v-model="currentPage" />
-    <div>currentPage: {{currentPage}}</div>
-  </div>
+    <div>
+        <template v-if="childDataLoaded">
+          <div>
+            <b-table striped hover :items="items" :fields="fields"  :current-page="currentPage" :per-page="perPage"></b-table>
+            <b-pagination align="center" size="md" :total-rows="items.length" :per-page="perPage" v-model="currentPage" />
+            <div>currentPage: {{currentPage}}</div>
+          </div>
+        </template>
+    </div>
 </template>
 
 <script>
@@ -12,49 +16,66 @@
 import axios from 'axios'
 
 export default {
+    methods:{
+
+fetchINitData(){
+
+    axios.get('http://laravel-angular-4-app/api/v1/students').then(response => this.items = response['data']);
+            this.childDataLoaded = true;
+        }
+    },
     data () {
-        let items_list= [
-            { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald', address: { country: 'USA', city: 'New York' } },
-            { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw', address: { country: 'Canada', city: 'Toronto' }},
-            { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson', address: { country: 'Australia', city: 'Sydney' } },
-            { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney', address: { country: 'England', city: 'London' } }
-        ];
+
         return {
+
+            childDataLoaded: false,
             currentPage: 1,
             perPage: 2,
             // Note 'isActive' is left out and will not appear in the rendered table
             fields: {
-                last_name: {
-                    label: 'Person last name',
+                id: {
+                    label: 'ID',
                     sortable: true
                 },
-                first_name: {
-                    label: 'Person first name',
+                name: {
+                    label: 'Person Name',
+                    sortable: true
+                },
+                email: {
+                    label: 'Person Email',
                     sortable: false
                 },
-                foo: {
-                    // This key overrides `foo`!
-                    key: 'age',
-                    label: 'Person age',
-                    sortable: true
+                admission_date: {
+                    label: 'Admission Date',
+                    sortable: false
                 },
-                city: {
-                    key: 'address.city'
+                faculty: {
+                    label: 'Faculty',
+                    sortable: false
                 },
-                'address.country': {
-                    label: 'Country'
+                major: {
+                    label: 'Major',
+                    sortable: false
+                },
+                country: {
+                    label: 'Country',
+                    sortable: false
+                },
+                created_at: {
+                    label: 'Created At',
+                    sortable: false
+                },
+                updated_at: {
+                    label: 'Updated At',
+                    sortable: false
                 }
             },
-            items: items_list
+            items: [],
+
         }
     },
     mounted() {
-        axios
-            .get('http://laravel-angular-4-app/api/v1/students')
-            .then(response => (console.log(response)))
-            .catch(e  => {
-                console.log(e);
-            });
+        this.fetchINitData();
     }
 }
 </script>
