0f617500da60eb81646c0ab8cdb900c69cee9a31
Inna Tarasyan <innatarasyanmail@gmail.com>
2018-09-06 15:56:42 +0500

    code enhancement

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,81 +1,156 @@
-
 <template>
-    <div>
-        <template v-if="childDataLoaded">
-          <div>
-            <b-table striped hover :items="items" :fields="fields"  :current-page="currentPage" :per-page="perPage"></b-table>
-            <b-pagination align="center" size="md" :total-rows="items.length" :per-page="perPage" v-model="currentPage" />
-            <div>currentPage: {{currentPage}}</div>
-          </div>
-        </template>
-    </div>
+    <b-container fluid>
+        <!-- User Interface controls -->
+        <b-row>
+            <b-col md="6" class="my-1">
+                <b-form-group horizontal label="Filter" class="mb-0">
+                    <b-input-group>
+                        <b-form-input v-model="filter" placeholder="Type to Search" />
+                        <b-input-group-append>
+                            <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
+            <b-col md="6" class="my-1">
+                <b-form-group horizontal label="Sort" class="mb-0">
+                    <b-input-group>
+                        <b-form-select v-model="sortBy" :options="sortOptions">
+                            <option slot="first" :value="null">-- none --</option>
+                        </b-form-select>
+                        <b-form-select :disabled="!sortBy" v-model="sortDesc" slot="append">
+                            <option :value="false">Asc</option>
+                            <option :value="true">Desc</option>
+                        </b-form-select>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
+            <b-col md="6" class="my-1">
+                <b-form-group horizontal label="Sort direction" class="mb-0">
+                    <b-input-group>
+                        <b-form-select v-model="sortDirection" slot="append">
+                            <option value="asc">Asc</option>
+                            <option value="desc">Desc</option>
+                            <option value="last">Last</option>
+                        </b-form-select>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
+            <b-col md="6" class="my-1">
+                <b-form-group horizontal label="Per page" class="mb-0">
+                    <b-form-select :options="pageOptions" v-model="perPage" />
+                </b-form-group>
+            </b-col>
+        </b-row>
+
+        <!-- Main table element -->
+        <b-table show-empty
+                 stacked="md"
+                 :items="items"
+                 :fields="fields"
+                 :current-page="currentPage"
+                 :per-page="perPage"
+                 :filter="filter"
+                 :sort-by.sync="sortBy"
+                 :sort-desc.sync="sortDesc"
+                 :sort-direction="sortDirection"
+                 @filtered="onFiltered"
+        >
+            <template slot="name" slot-scope="row">{{row.value}} </template>
+            <template slot="isActive" slot-scope="row">{{row.value?'Yes :)':'No :('}}</template>
+            <template slot="actions" slot-scope="row">
+                <!-- We use @click.stop here to prevent a 'row-clicked' event from also happening -->
+                <b-button size="sm" @click.stop="info(row.item, row.index, $event.target)" class="mr-1">
+                    Info modal
+                </b-button>
+                <b-button size="sm" @click.stop="row.toggleDetails">
+                    {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+                </b-button>
+            </template>
+            <template slot="row-details" slot-scope="row">
+                <b-card>
+                    <ul>
+                        <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value}}</li>
+                    </ul>
+                </b-card>
+            </template>
+        </b-table>
+
+        <b-row>
+            <b-col md="6" class="my-1">
+                <b-pagination align="center" :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0" />
+            </b-col>
+        </b-row>
+
+        <!-- Info modal -->
+        <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
+            <pre>{{ modalInfo.content }}</pre>
+        </b-modal>
+
+    </b-container>
 </template>
 
 <script>
+    import axios from 'axios'
 
-import axios from 'axios'
+    const items = [
 
-export default {
-    methods:{
+    ];
 
-fetchINitData(){
+    export default {
+        data () {
+            return {
+                items: [],
+                fields: [
+                    { key: 'name', label: 'Person Name', sortable: true, sortDirection: 'desc' },
+                    { key: 'email', label: 'Person Email', sortable: true, sortDirection: 'desc' },
+                    { key: 'admission_date', label: 'Admission Date', sortable: true, sortDirection: 'desc' },
+                    { key: 'actions', label: 'Actions' }
 
-    axios.get('http://laravel-angular-4-app/api/v1/students').then(response => this.items = response['data']);
-            this.childDataLoaded = true;
-        }
-    },
-    data () {
-
-        return {
-
-            childDataLoaded: false,
-            currentPage: 1,
-            perPage: 2,
-            // Note 'isActive' is left out and will not appear in the rendered table
-            fields: {
-                id: {
-                    label: 'ID',
-                    sortable: true
-                },
-                name: {
-                    label: 'Person Name',
-                    sortable: true
-                },
-                email: {
-                    label: 'Person Email',
-                    sortable: false
-                },
-                admission_date: {
-                    label: 'Admission Date',
-                    sortable: false
-                },
-                faculty: {
-                    label: 'Faculty',
-                    sortable: false
-                },
-                major: {
-                    label: 'Major',
-                    sortable: false
-                },
-                country: {
-                    label: 'Country',
-                    sortable: false
-                },
-                created_at: {
-                    label: 'Created At',
-                    sortable: false
-                },
-                updated_at: {
-                    label: 'Updated At',
-                    sortable: false
-                }
+                ],
+                currentPage: 1,
+                perPage: 2,
+                totalRows: items.length,
+                pageOptions: [ 2, 5, 10, 15 ],
+                sortBy: null,
+                sortDesc: false,
+                sortDirection: 'asc',
+                filter: null,
+                modalInfo: { title: '', content: '' }
+            }
+        },
+        computed: {
+            sortOptions () {
+                // Create an options list from our fields
+                return this.fields
+                    .filter(f => f.sortable)
+                    .map(f => { return { text: f.label, value: f.key } })
+            }
+        },
+        methods: {
+            info (item, index, button) {
+                this.modalInfo.title = `Row index: ${index}`
+                this.modalInfo.content = JSON.stringify(item, null, 2)
+                this.$root.$emit('bv::show::modal', 'modalInfo', button)
             },
-            items: [],
-
-        }
-    },
-    mounted() {
-        this.fetchINitData();
+            resetModal () {
+                this.modalInfo.title = ''
+                this.modalInfo.content = ''
+            },
+            onFiltered (filteredItems) {
+                // Trigger pagination to update the number of buttons/pages due to filtering
+                this.totalRows = filteredItems.length
+                this.currentPage = 1
+            },
+            fetchINitData(){
+               axios.get('http://laravel-angular-4-app/api/v1/students').then(response => this.items = response['data']);
+               this.childDataLoaded = true;
+            }
+        },
+        mounted() {
+           this.fetchINitData();
+        },
     }
-}
 </script>
+
+<!-- table-complete-1.vue -->
